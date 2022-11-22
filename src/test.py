55dc7efbcea87c52e467058c2c43779fc23e35f5
Siddharth <st22041@ormiston.school.nz>
2022-11-22 13:18:07 +1300

    Added more files, foundation for character detection

--- src/test.py
+++ src/test.py
@@ -6,14 +6,15 @@ url = lucky(textName)
 print(url)
 
 r = requests.get(url)
-if r.status_code >= 200 and r.status_code < 300:
+
+if str(r.status_code)[0] == "2":
+    
     downloaded = trafilatura.fetch_url(url)
     text = trafilatura.extract(downloaded)
 
-
-    with open("e.txt","w", encoding="utf-8") as file:
+    with open("extractedContent.txt", "w", encoding="utf-8") as file:
         file.write(text)
-    os.replace(f"{os.getcwd()}\\e.txt", f"{os.getcwd()}\\data\\e.txt")
+    os.replace(f"{os.getcwd()}\\extractedContent.txt", f"{os.getcwd()}\\data\\extractedContent.txt")
 
     splitText = text.split("|")
 
@@ -26,6 +27,21 @@ if r.status_code >= 200 and r.status_code < 300:
                     checkCommit = True
 
     os.replace(f"{os.getcwd()}\\eee.txt", f"{os.getcwd()}\\data\\eee.txt")
+
+    listedText = text.split(" ")
+    testList = []
+
+    with open("ee.txt", "w", encoding='utf-8') as file:
+        for i in listedText:
+            if  re.search("characters", i.lower()) != None:
+                print("EEEEEEEEEEEEEEEEEEE")
+                testList.append(i)
+
+        with open("test.txt", "w", encoding="utf-8") as testFile:
+            testFile.write(str(testList))
+
+        file.write(str(listedText))
+    os.replace(f"{os.getcwd()}\\ee.txt", f"{os.getcwd()}\\data\\ee.txt")
 
 else:
     print("Problem accessing website")
